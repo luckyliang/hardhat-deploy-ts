23853879ab20d8064e4ec582aa6e3388704558f0
ronin66666 <1037895153@qq.com>
2022-09-29 18:06:42 +0800

    update

--- utils/eip712.ts
+++ utils/eip712.ts
@@ -1,5 +1,5 @@
 
-import { defaultAbiCoder, keccak256, recoverAddress, solidityPack, toUtf8Bytes } from "ethers/lib/utils";
+import { defaultAbiCoder, keccak256, recoverAddress, solidityPack, toUtf8Bytes, joinSignature } from "ethers/lib/utils";
 import { ecsign } from "ethereumjs-util";
 import {  ethers } from "hardhat";
 import { Signature } from "ethers";
@@ -35,6 +35,9 @@ export function domainSeparatorV4(
 }
 
 // typeDataHash
+//domainSeparator: domainSeparatorV4()
+//structHash: 自定义签名数据
+//返回用于签名的hash
 export function toTypedDataHash(domainSeparator: string, structHash: string): string {
     return keccak256(
         solidityPack(
@@ -44,10 +47,12 @@ export function toTypedDataHash(domainSeparator: string, structHash: string): st
         )
 }
 
+//TUDO
 export async function signWithAddress(typeDataHash: string, address: string): Promise<Signature> {
 
     // 获取hardhat 中的signer
     const signer = await ethers.getSigner(address)
+
 
     //签名消息
     const message = await signer.signMessage(typeDataHash);
@@ -61,11 +66,19 @@ export async function signWithAddress(typeDataHash: string, address: string): Pr
     // console.log(`signatureLike = ${signatureLike}`);
     
     return sign;
-    
 }
 
-// 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
-export function signWithPrivateKey(typeDataHash: string, privateKey: string): SignatureLike {
+/**
+ * 通过私钥签名
+ * @param _typeDataHash 用于签名的hash
+ * @param _privateKey 私钥
+ * @returns 用于712验证，SignatureLike.v SignatureLike.r SignatureLike.s
+ * function recover(bytes32 hash,uint8 v,bytes32 r,bytes32 s) internal pure returns (address)
+ */
+export function signWithPrivateKey(_typeDataHash: string, _privateKey: string): SignatureLike {
+    
+    const typeDataHash = _typeDataHash.startsWith("0x") ? _typeDataHash.slice(2) : _typeDataHash;
+    const privateKey = _privateKey.startsWith("0x") ? _privateKey.slice(2) : _privateKey;
     
     const signature = ecsign(Buffer.from(typeDataHash.slice(2), 'hex'), Buffer.from(privateKey.slice(2),'hex'))
     console.log("signature = ", signature);
@@ -76,6 +89,19 @@ export function signWithPrivateKey(typeDataHash: string, privateKey: string): Si
         v: signature.v
     }
 }
+
+
+/**
+ * 
+ * @param signatureLike 签名SignatureLike类型
+ * @returns bytes 用于712 验证 时传入signature
+ * @openzeppelin/contract/utils/cryptography/ECDSA.sol 以下方法传入的signature
+ * function recover(bytes32 hash, bytes memory signature) internal pure returns (address)
+ */
+export function signatureLikeToBytesString(signatureLike: SignatureLike): string {
+    return joinSignature(signatureLike)
+}
+
 
 
 
