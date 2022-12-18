d9f17b8dfa1816bbcc9be5fef9d56b858f2b5f53
ronin66666 <1037895153@qq.com>
2022-12-18 13:42:58 +0800

    update

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,7 +1,7 @@
 // contracts/Box.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
- import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/finance/VestingWallet.sol";
 contract Box {
     
     uint256 private value;
